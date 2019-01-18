b0cb3e346021da445e30520fcbab7086a1c0a445
HRSA\KRedding <kredding@hrsa.gov>
2019-01-18 15:37:15 -0500

    update Arnold

--- public/electron.js
+++ public/electron.js
@@ -1,20 +1,19 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
 
+var si = require('systeminformation');
 
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({width: 1100, height: 800, webPreferences: { webSecurity: false}});
-    // Dev Tools
-    // mainWindow.webContents.openDevTools();
-
+    
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   
-    mainWindow.on('closed', () => mainWindow = null);
+    mainWindow.on('closed', () => mainWindow = null);    
 }
 
 app.on('ready', createWindow);
@@ -22,5 +21,23 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+/* Render Processes */
+ipcMain.on('arnold-info-msg', (event, arg) => {  
+  var systype = (arg ? arg : null);
+  //console.log("Received: ", systype);
+  
+  switch(systype){
+    case 'all':
+      si.getAllData(function(data) { event.sender.send('arnold-info-reply', {"data":data}); });
+      break;
+    case 'cpu':    
+      si.cpu(function(data) { event.sender.send('arnold-info-reply', {"data":data}); });
+      break;        
+    default:
+      event.sender.send('arnold-info-reply', {"error":"No Data [1]"});
+      break;
   }
 });
--- src/js/templates/arnold.js
+++ src/js/templates/arnold.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-var si = require('systeminformation');
-var os = require('os');
+const { ipcRenderer } = window.require("electron");
+
+//var os = require('os');
 
 class Arnold extends Component{
     constructor(props) {
@@ -8,45 +9,105 @@ class Arnold extends Component{
 
         this.state = {
             testSz: [1,2,3,4,5],
-            systeminfo: null,
+            system: null,
             os:null,
-            battery:null
-        }        
+            cpu:null,
+            cpuCurrentspeed:null,
+            users:null,
+            temp:null,
+            mem:null,
+            battery:null,
+            fsSize:null
+        }
+
+        this.getBatteryLvl = this.getBatteryLvl.bind(this);
     }
     
 
     render(){        
         return(
             <div className="body-container arnold-body">
-                <div className="horizontal-view hex-grid">
-                    {/* <div className="horizontal-container hex-grid">*/}
-                        <div className="grid-row"> 
-                            {this.state.testSz.map((item, i) =>                                             
-                                    <div className="hex" key={i}>
-                                        <div className="hexIn">
-                                            <div className="hexBody">
-                                                <div className="hex-body-container">
-                                                    <p>T{i}</p>
-                                                </div>
+                <div className="horizontal-view hex-grid">                    
+                    <div className="grid-row"> 
+                        {/* System Information*/}                         
+                        <div className="hex">
+                            <div className="hexIn">
+                                <div className="hexBody">
+                                    <div className="hex-body-container">
+                                        <div className="hex-inner-container">                                        
+                                            <div className={"txt-line blur_line" + (!this.state.system ? " blur" : "")}>
+                                                <span>{(this.state.system ? this.state.system.manufacturer : "No Manufacturer Data")}</span>
+                                                <span>{(this.state.system ? this.state.system.model : "N/A")}</span>
+                                                <span>{(this.state.system ? this.state.system.version : "0.0.0")}</span>
+                                            </div>
+
+                                            <div className={"txt-line blur_line" + (!this.state.os ? " blur" : "")}>
+                                                <span>{(this.state.os ? this.state.os.platform : "No Platform Data")}</span>
+                                                <span>{(this.state.os ? "("+this.state.os.arch+")" : "N/A")}</span>
+                                                <span>{(this.state.os ? this.state.os.release : "0.0.0.0")}</span>
+                                            </div>                                                                                                                                                                                
+                                            <div className="battery-icon">
+                                                <div className="status" style={{width: this.getBatteryLvl(this.state.battery)}}></div>
+                                                <span className="statusVal">{this.getBatteryLvl(this.state.battery)}</span>
                                             </div>
                                         </div>
-                                    </div>   
-                            )}
-                        </div> 
-                        <div className="grid-row"> 
-                            {this.state.testSz.map((item, i) =>                                             
-                                    <div className="hex" key={i}>
-                                        <div className="hexIn">
-                                            <div className="hexBody">
-                                                <div className="hex-body-container">
-                                                    <p>T{i}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>   
-                            )}
+                                    </div>
+                                </div>
+                            </div>
                         </div>   
-                    {/*</div>*/}
+                        {/* CPU Info */}                         
+                        <div className="hex">
+                            <div className="hexIn">
+                                <div className="hexBody">
+                                    <div className="hex-body-container">
+                                        <p>CPU Information</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                        {/* Network Information*/}                         
+                        <div className="hex">
+                            <div className="hexIn">
+                                <div className="hexBody">
+                                    <div className="hex-body-container">
+                                        <p>Network Information</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                                                      
+                    </div> 
+                    <div className="grid-row"> 
+                            {/* Memory Information*/}                         
+                        <div className="hex">
+                            <div className="hexIn">
+                                <div className="hexBody">
+                                    <div className="hex-body-container">
+                                        <p>Memory Information</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                        {/* Disk Info */}                         
+                        <div className="hex">
+                            <div className="hexIn">
+                                <div className="hexBody">
+                                    <div className="hex-body-container">
+                                        <p>Disk Information</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                        {/* Tempurature Information*/}                         
+                        <div className="hex">
+                            <div className="hexIn">
+                                <div className="hexBody">
+                                    <div className="hex-body-container">
+                                        <p>Tempurature Information</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                    </div>                       
                 </div>
             </div>
         );        
@@ -55,8 +116,8 @@ class Arnold extends Component{
     componentDidMount(){
         let self = this;
         
-        try{
-            console.log("1] ");
+        try{      
+            self.initRender();             
             self.getAllDesktopData();
         }
         catch(ex){
@@ -64,17 +125,65 @@ class Arnold extends Component{
         }
     }
 
+    componentWillUnmount() {
+        console.log("Removing ... ");
+        ipcRenderer.removeListener('arnold-info-reply', this.initRender);
+    }
+
     getAllDesktopData(){
         try{
-            console.log("2] ");
-            si.getAllData().then((data) =>  {
-                console.log('All-Information:');
-                console.log(data);
-            }).catch(error => console.error(error));
-            console.log("3] ", os.platform(), " | ", os.release());
+            ipcRenderer.send('arnold-info-msg', 'all');
         }
         catch(ex){
             console.log(" Error getting desktop data: ", ex)
+        }
+    }
+
+    initRender(){  
+        var self = this;      
+        try {
+            ipcRenderer.on('arnold-info-reply', (event, arg) => { self.buildInfoObjects(arg) });
+        }
+        catch(ex){
+            console.log(" [Arnold] Error: ", ex);
+        }
+    }
+
+    getBatteryLvl(battery){        
+        var retLvl = 0;
+        try {
+            if(!battery){
+                retLvl = 5;
+            }
+            else if(battery.percent >= 100){
+                retLvl = 100;
+            }
+            else {
+                retLvl = battery.percent;
+            }
+        }
+        catch(ex){
+            console.log(" [Arnold] Error: ", ex);
+        }
+
+        return retLvl + '%';
+    }
+    /* Build Data Object */
+    buildInfoObjects(data){
+        var self = this;
+        var dataObjs = ["system", "os", "cpu", "net", "cpuCurrentspeed", "users", "temp", "mem", "battery", "fsSize"];
+        try {
+            if(!data.data){ console.log(" [Arnold] Error: No data"); }
+            else {
+                for(var i =0; i < dataObjs.length; i++){
+                    if(dataObjs[i] in data.data){
+                        this.setState({ [dataObjs[i]]: data.data[dataObjs[i]] });
+                    }
+                }
+            }
+        }
+        catch(ex){
+            console.log(" [Arnold] Error: ", ex);
         }
     }
 }
